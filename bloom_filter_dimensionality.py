20e2c018834d3fd7849f2e9e9db6e08ad7262351
Alexander Jiang <dashuai99@gmail.com>
2019-03-22 01:12:35 -0500

    Dimensionality results: dimensionality doesn't seem to affect FP rate

--- bloom_filter_dimensionality.py
+++ bloom_filter_dimensionality.py
@@ -70,9 +70,9 @@ def generate_random_vectors(vector_dim=100, num_vectors=100000):
 
 def main():
     random.seed(99423)
-    vector_dim = 100
+    vector_dim = 300
     num_vectors = 100000
-    desired_fp_rate = 0.001
+    desired_fp_rate = 0.01
 
     vectors = generate_random_vectors(vector_dim=vector_dim, num_vectors=num_vectors)
     bf = BloomFilter(num_vectors, desired_fp_rate, universal_hash_vectors, vector_dim)
@@ -83,13 +83,26 @@ def main():
         bf.insert(vec)
         count += 1
 
-    count = 0
-    for vec in vectors:
-        if count % 1000 == 0:
-            print(f"tested {count} / {num_vectors}")
-        if not bf.test(vec):
-            print(f"ERROR false negative detected in vanilla Bloom filter!!")
-            print(f"key: {vec}")
-        count += 1
+    # count = 0
+    # for vec in vectors:
+    #     if count % 1000 == 0:
+    #         print(f"tested {count} / {num_vectors}")
+    #     if not bf.test(vec):
+    #         print(f"ERROR false negative detected in vanilla Bloom filter!!")
+    #         print(f"key: {vec}")
+    #     count += 1
+
+    num_test_vectors = 5000
+    test_vectors = generate_random_vectors(vector_dim=vector_dim, num_vectors=num_test_vectors)
+    fp_count = 0
+    for test_vec in test_vectors:
+        if test_vec in vectors:
+            num_test_vectors -= 1
+            continue
+        if bf.test(test_vec):
+            fp_count += 1
+    fp_rate = float(fp_count) / num_test_vectors
+    print("Actual fp rate: ", fp_rate)
+    print("Expected fp rate: ", desired_fp_rate)
 
 if __name__ == "__main__": main()
